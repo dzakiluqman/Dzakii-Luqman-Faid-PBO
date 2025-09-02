824396d763587fcb5eb0bbac08d2407c91c4257f
dzakiluqman <dzakiluqman275@gmail.com>
2025-09-02 08:09:15 +0700

    Commit Project1

---
+++ Project1/dzakii_project1/src/main/java/com/mycompany/dzakii_project1/Dzakii_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dzakii_project1;
+
+/**
+ *
+ * @author usER
+ */
+public class Dzakii_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Dzakii Luqman Faid");
+        System.out.println("NPM    : 2410506016");
+        System.out.println("Alamat : Jl Nanas");
+        System.out.println("No HP  : 080808080808");
+    }
+}
