b753d23829b8a4f77c31858dff482a97d354a838
dzakiluqman <dzakiluqman275@gmail.com>
2025-11-04 08:01:08 +0700

    Praktikum PBO 9
    Minggu ke-9 mata kuliah Praktikum Pemrograman Berorientasi Objek.

---
+++ PraktikumPBO_9/src/main/java/praktikum9/Anjing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum9;
+
+/**
+ *
+ * @author usER
+ */
+class Anjing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Anjing mengeluarkan suara: Guk Guk");
+    }
+}
---
+++ PraktikumPBO_9/src/main/java/praktikum9/PraktikumPBO_9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package praktikum9;
+
+/**
+ *
+ * @author usER
+ */
+public class PraktikumPBO_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
